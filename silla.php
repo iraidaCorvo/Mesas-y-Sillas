f23edf990e8c573b545772e3b9da07ccadc7cdbb
iraidaCorvo <iraida.arcorvo@gmail.com>
2018-05-25 00:12:59 +0100

    __set __get for extra variables

--- silla.php
+++ silla.php
@@ -1,35 +1,49 @@
 <?php
 class silla extends mueble{
+    
     private $numpatas='';
     private $respaldo='';
     private $cojin='';
 
     function __construct($color, $material){
         parent::__construct($color, $material);
+        
     }
-
     function __get($name){
-        switch($name){
+        switch($name):
             case 'Numpatas':
                 return $this->numpatas;
-            
-        }
-    
+            case 'Respaldo':
+                return $this->respaldo;
+            case 'Cojin':
+                return $this->cojin;   
+        endswitch;
     }
+
     function __set($name, $value){
-        switch($name){
+        switch($name):
             case 'Numpatas':
-                return $this->numpatas=$value;   
-        }
+                return $this->numpatas = $value; 
+                break;
+            case 'Respaldo':
+                return $this->respaldo=$value;
+                break;
+            case 'Cojin':
+                return $this->cojin=$value;   
+                break;
+                 
+        endswitch;
     }
+    
     public function printCaracteristicas(){
+    
         echo '<br>';
         echo 'SILLA:';
         echo '<br>';
         parent::printCaracteristicas();
         echo 'Respaldo: '. $this->respaldo;
         echo '<br>';
-        echo 'Numero Patas: '. $this->numPatas;
+        echo 'Numero Patas: '. $this->NumPatas;
         echo '<br>';
         echo 'Cojin: '. $this->cojin;
         echo '<br>';
